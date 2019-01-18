594e99ac1bec5a3320204fb3795c695233a9962c
mjink001 <matthew.jinkens@7-11.com>
2019-01-18 06:49:30 -0600

    Update calendar stlyes

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,22 +1,52 @@
 import React, { PureComponent } from 'react'
 import CalendarComponent from 'react-calendar'
 import { CalendarTile } from './CalendarTile'
+import { Typography } from '@material-ui/core'
+import { ChevronLeft, ChevronRight, FirstPage, LastPage } from '@material-ui/icons'
 import './styles.scss'
 
 export class Calendar extends PureComponent {
+    state = {
+        currentMonth: null,
+        calendarValue: null
+    }
+
     _onClickDay = value => {
-        console.log(value)
+    }
+
+    _navigationLabel = ({ label }) => <Typography variant='h6' align='center' style={{ color: 'rgb(225, 59, 30)' }}>{label}</Typography>
+
+    _tileContent = ({ date }) => {
+        const { innerWidth } = this.props
+
+        return <CalendarTile date={date.getTime().toString()} innerWidth={innerWidth} />
+    }
+
+    componentDidMount() {
+        const currentMonth = new Date().toString().split(' ').slice(1, 4).reduce((acc, val, index) => {
+            return acc + (index === 1 ? '' : val)
+        }, '').toLowerCase()
+
+        this.setState({
+            currentMonth,
+            calendarValue: new Date()
+        })
     }
 
     render() {
+        const { calendarValue } = this.state
         return (
             <CalendarComponent
                 className="calendar"
                 minDetail="month"
-                showFixedNumberOfWeeks={true}
-                // navigationLabel={() => null}
-                tileContent={<CalendarTile />}
+                navigationLabel={this._navigationLabel}
+                nextLabel={<ChevronRight />}
+                next2Label={<LastPage />}
+                prevLabel={<ChevronLeft />}
+                prev2Label={<FirstPage />}
+                tileContent={this._tileContent}
                 onClickDay={this._onClickDay}
+                value={calendarValue}
             />
         )
     }
--- src/components/calendar/CalendarTile.js
+++ src/components/calendar/CalendarTile.js
@@ -1,14 +1,34 @@
 import React from 'react'
-import AssignmentIcon from '@material-ui/icons/Assignment'
-import EventIcon from '@material-ui/icons/Event'
+import { CalendarChip } from './CalendarChip'
+import { Tooltip } from '@material-ui/core'
 import InfoIcon from '@material-ui/icons/Info'
 
-export const CalendarTile = ({ assignment, plannedEvent, misc }) => (
+export const CalendarTile = ({ date, assignmentCount, eventCount, miscCount, innerWidth }) => (
     <div className="tile">
-        <div className="tile-icons">
-            {assignment && <AssignmentIcon />}
-            {plannedEvent && <EventIcon />}
-            {misc && <InfoIcon />}
-        </div>
+        {
+            innerWidth > 667 ?
+                <div className="tile-icons">
+                    {
+                        assignmentCount > 0 &&
+                        <CalendarChip title='Assignments' count={assignmentCount} />
+                    }
+                    {
+                        eventCount > 0 &&
+                        <CalendarChip title='Events' count={eventCount} />
+                    }
+                    {
+                        miscCount > 0 &&
+                        <CalendarChip title='Misc.' count={miscCount} />
+                    }
+                </div>
+                :
+                (assignmentCount > 0 || eventCount > 0 || miscCount > 0) &&
+                <div>
+                    <Tooltip title={'Click to view'} enterDelay={300} enterTouchDelay={300}>
+                        <InfoIcon style={{ color: 'rgb(225, 59, 30)' }} />
+                    </Tooltip>
+                </div>
+        }
     </div>
+
 )
